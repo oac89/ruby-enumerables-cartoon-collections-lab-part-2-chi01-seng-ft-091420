0189223073f61bc0669486a446fbff8b3a6b0361
Oscar A Castro <oacastro2@gmail.com>
2020-09-06 14:52:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_valid_calls(planeteer_calls)
-      valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+      
 
       planeteer_calls.find do |element| element == [valid_calls] 
     end 
