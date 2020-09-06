a3d25a5845da7e8d01fe52ce49673b9daf98e361
Oscar A Castro <oacastro2@gmail.com>
2020-09-06 15:34:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ def find_valid_calls(planeteer_calls)
       valid_calls = [ "Earth!", "Wind!", "Fire!", "Water!", "Heart!" ]
 
       planeteer_calls.include?(valid_calls) do 
-        |element| element
+        |element| element == String 
   
     end 
 
